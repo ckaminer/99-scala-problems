48d2d5457a3e641f904ee83439f0ea430adc2e01
Charles Kaminer <ckamine@g.clemson.edu>
2016-12-06 22:20:46 -0500

    complete second problem find second to last item in list using recursion

--- src/main/scala/problems.scala
+++ src/main/scala/problems.scala
@@ -4,4 +4,10 @@ object Problem {
     case _ :: tail   => lastWithRecursion(tail)
     case Nil         => throw new NoSuchElementException
   }
+
+  def secondToLastWithRecursion[A](ls: List[A]): A = ls match {
+    case a :: _ :: Nil => a
+    case _ :: tail     => secondToLastWithRecursion(tail)
+    case Nil           => throw new NoSuchElementException
+  }
 }
--- src/test/scala/problems_test.scala
+++ src/test/scala/problems_test.scala
@@ -4,4 +4,9 @@ class ProblemTest extends FunSuite with Matchers {
   test("recursively find the last element of a list") {
     Problem.lastWithRecursion(List(1, "a", 7)) should be (7)
   }
+
+  test("recursively find the last but one element of a list") {
+    Problem.secondToLastWithRecursion(List(1,2,3,4)) should be (3)
+  }
+
 }
